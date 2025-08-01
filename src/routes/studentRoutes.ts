b34c7eddadb79a6cddefbc91b67cdf7c41bf34ec
Anany More <anany.more@ananyamore-vrs-13.successivetech.com>
2025-08-01 12:51:24 +0530

    completed 90%

--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { registerStudent, studentSignin } from "../controllers/students";
+import { filterStudent, registerStudent, studentSignin } from "../controllers/students";
 import validateStudent from "../utils/shcemaValidator/validateStudent";
 import authenticate from "../middleware/authenticate"
 const studentRouter = express.Router();
 
 studentRouter.post("/register", validateStudent, registerStudent);
 studentRouter.post("/signin", authenticate, studentSignin);
-studentRouter.get("/");
+studentRouter.get("/filter-student", filterStudent);
